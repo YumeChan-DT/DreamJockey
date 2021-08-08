8b89b528f02eca36cabd3b03e2f4959f510957d9
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-08-08 18:43:05 +0200

    Init Solution, Project, and PluginManifest

---
+++ PluginManifest.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using YumeChan.PluginBase;
+
+namespace YumeChan.DreamJockey
+{
+	public class PluginManifest : Plugin
+	{
+		public override string PluginDisplayName => "YumeChan DreamJockey";
+		public override bool PluginStealth => false;
+
+		public override Task LoadPlugin()
+		{
+			return base.LoadPlugin();
+		}
+
+		public override Task UnloadPlugin()
+		{
+			return base.UnloadPlugin();
+		}
+	}
+}
