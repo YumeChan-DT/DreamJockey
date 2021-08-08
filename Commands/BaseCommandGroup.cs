a020f12743b6362d05b22712a1214c369dd928cf
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-08-08 19:09:46 +0200

    Draft Commands structure

---
+++ Commands/BaseCommandGroup.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YumeChan.DreamJockey.Commands
+{
+	[Group("dreamjockey"), Aliases("dj", "music")]
+	public partial class BaseCommandGroup : BaseCommandModule
+	{
+		[Command("join")]
+		public async Task JoinAsync(CommandContext ctx, DiscordChannel channel)
+		{
+
+		}
+
+		[Command("leave")]
+		public async Task LeaveAsync(CommandContext ctx, DiscordChannel channel)
+		{
+
+		}
+
+		[Command("play")]
+		public async Task PlayAsync(CommandContext ctx, [RemainingText] string search)
+		{
+
+		}
+		[Command]
+		public async Task PlayAsync(CommandContext ctx, Uri url)
+		{
+
+		}
+
+		[Command("pause")]
+		public async Task PauseAsync(CommandContext ctx)
+		{
+
+		}
+	}
+}
